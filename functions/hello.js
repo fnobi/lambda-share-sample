f243830a9f50cc10b9899a7c3415673032bc50f6
nobii <fujisawa-shin@kayac.com>
2018-08-04 00:59:46 +0900

    json apiにしてみる

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,13 @@
-exports.handler = function(event, context, callback) {
+exports.handler = function({ path, queryStringParameters }, context, callback) {
+    const res = {
+        path: event.path,
+        id: queryStringParameters.id,
+    };
     callback(null, {
         statusCode: 200,
-        body: event.path
+        headers: {
+            'Content-Type': 'application.json',
+        },
+        body: JSON.stringify(res)
     });
 }
