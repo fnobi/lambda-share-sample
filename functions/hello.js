b718d9129dc6a254fac84833d2ef0e09d5d8f3d6
nobii <fujisawa-shin@kayac.com>
2018-08-04 01:12:19 +0900

    更に色々こねる

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,29 @@
-exports.handler = function({ path, queryStringParameters }, context, callback) {
+const randomstring = require("randomstring");
+
+exports.handler = function({
+    path,
+    queryStringParameters
+}, context, callback) {
+    const id = queryStringParameters.id;
+    if (isNaN(id)) {
+        callback(null, {
+            statusCode: 400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ error: 'invalid id'})
+        });
+        return;
+    }
     const res = {
         path,
-        id: queryStringParameters.id,
+        id,
+        chara: randomstring.generate(),
     };
     callback(null, {
         statusCode: 200,
         headers: {
-            'Content-Type': 'application.json',
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(res)
     });
